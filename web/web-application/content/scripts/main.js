bd486d5e766eb314f480fd6860e2c26c7e2955a7
marinoscar <oscar@marin.cr>
2019-09-16 18:22:09 -0500

    initial commit

---
+++ web/web-application/content/scripts/main.js
@@ -0,0 +1,17 @@
+﻿var mainApp = {
+    onRegister: function () {
+        window.location.href = './success.html';
+    },
+    onCancel: function () {
+        window.location.href = './failed.html';
+    }
+}
+
+$(document).ready(function () {
+    $('#btn-register').on('click', function () {
+        mainApp.onRegister();
+    });
+    $('#btn-cancel').on('click', function () {
+        mainApp.onCancel();
+    });
+});
